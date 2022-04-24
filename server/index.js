4d66129118cd04c25c8ac5fcd318b868ee59754d
TechGenius7777 <supratikmahato7777@gmail.com>
2022-04-24 14:30:19 +0530

    CORS Error Fix

--- server/index.js
+++ server/index.js
@@ -18,7 +18,11 @@ try {
   console.error(error);
 }
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/", rootRoute);
