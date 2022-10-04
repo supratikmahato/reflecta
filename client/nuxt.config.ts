cb032c1cf3a23ec8638dd1899074b512a74bf2d7
Tech Genius <74496810+TechGenius7777@users.noreply.github.com>
2022-10-04 19:31:27 +0530

    Minor bug fix

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
       link: [
         { rel: "icon", type: "image/x-icon", href: "/favicon.svg" },
         {
-          href: "https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css",
+          href: "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css",
           rel: "stylesheet",
         },
       ],
