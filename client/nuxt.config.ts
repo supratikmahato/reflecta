026311996546da23c47e405d73f584126f2bf257
TechGenius7777 <supratikmahato7777@gmail.com>
2022-11-29 17:35:08 +0530

    Feat Nuxt 3 Stable and much more!

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,17 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
   },
-  publicRuntimeConfig: {
-    baseUrl: process.env.BASE_URL || "http://localhost:8000",
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL || "http://localhost:8000",
+    },
   },
   css: ["@/styles/styles.css", "@/styles/fonts.css"],
   app: {
