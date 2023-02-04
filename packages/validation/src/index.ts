a2fac83cf940586f87909148871a41124eb73f79
TechGenius7777 <supratikmahato7777@gmail.com>
2023-02-04 12:39:16 +0530

    Package bug fix

---
+++ packages/validation/src/index.ts
@@ -0,0 +1,13 @@
+import userRegisterValidation from "./userRegister";
+import userLoginValidation from "./userLogin";
+import coffeePostValidation from "./coffeePost";
+import coffeePatchValidation from "./coffeePatch";
+import coffeeIsPublicValidation from "./coffeeIsPublic";
+
+export {
+  userRegisterValidation,
+  userLoginValidation,
+  coffeePostValidation,
+  coffeePatchValidation,
+  coffeeIsPublicValidation,
+};
