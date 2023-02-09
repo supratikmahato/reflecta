4a3adb64a4e80e78a4cddd20acf2a54f1719e8a1
TechGenius7777 <supratikmahato7777@gmail.com>
2023-02-09 22:38:22 +0530

    Client build bug fix trial

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+  build: {
+    transpile: ["validation"],
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
