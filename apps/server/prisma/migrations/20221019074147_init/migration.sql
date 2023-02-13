d9b5e98c2e087676ab1b59d12c64b9b3ae81bf6e
TechGenius7777 <supratikmahato7777@gmail.com>
2023-02-13 12:22:31 +0530

    Bug fixes

--- apps/server/prisma/migrations/20221019074147_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Mood" ADD COLUMN     "deletedAt" TIMESTAMP(3),
-ADD COLUMN     "updatedAt" TIMESTAMP(3);
--- apps/server/prisma/migrations/20221230125803_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Mood" ADD COLUMN     "isPublic" BOOLEAN NOT NULL DEFAULT false;
